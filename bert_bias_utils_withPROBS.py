669042bc8c21864ed15ecd8a7089ab8f417be05f
elgonher <93835313+elgonher@users.noreply.github.com>
2022-07-06 18:58:14 +0200

    Update bert_bias_utils_withPROBS.py

--- bert_bias_utils_withPROBS.py
+++ bert_bias_utils_withPROBS.py
@@ -66,7 +66,7 @@ def attention_mask_creator(input_ids):
 def asociaciones(group1):
     print('Group 1:')
     #print(group1.describe()) # returns a df with the statistics of ALL numerical columns in original df
-    print(group1.describe()['Pre_Assoc']).T # returns a df with the statistics of the column Pre_Assoc in original df
+    print(group1.describe()['Pre_Assoc'].T) # returns a df with the statistics of the column Pre_Assoc in original df
 
     return
 
